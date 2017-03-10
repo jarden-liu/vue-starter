309dc679c8c476c47dc3f4cc44d031afb25afa93
jarden-liu <liujunjiang@szxhinfo.com>
2017-03-10 15:30:29 +0800

    Add loading

--- app/app.vue
+++ app/app.vue
@@ -1,17 +1,17 @@
 <template>
-  <div>
+<div>
 
-    <div style="text-align:center;">
-      <img src="./assets/logo.jpg"/>
-    </div>
-    <div class="content">
-      <h1 class="title">WELCOME</h1>
-      <div >
-        {{message}}
-      </div>
-    </div>
-
+  <div style='text-align:center;'>
+    <img src='./assets/logo.jpg' />
   </div>
+  <div class='content'>
+    <h1 class='title'>WELCOME</h1>
+    <div>
+      {{message}}
+    </div>
+  </div>
+
+</div>
 </template>
 
 
@@ -19,21 +19,37 @@
 <script>
 let vm;
 
-export default  {
-  name:'app',
-  data(){
+export default {
+  name: 'app',
+  data() {
     return {
-      message:'Hello Vue!'
+      message: 'Hello Vue!'
+    };
+  },
+  mounted() {
+    this.appShow();
+  },
+  methods: {
+    appShow() {
+      const {
+        PAGE_START_TIME
+      } = window;
+      const PAGE_END_TIME = new Date().getTime() // 结束时间
+      const diffTime = PAGE_END_TIME - PAGE_START_TIME
+      const timer = setTimeout(() => {
+        clearTimeout(timer)
+        document.querySelector('.page-preloading ').className += ' hide';
+
+      }, diffTime > 1000 ? 0 : 1000 - diffTime);
     }
   }
+
 };
-
-
 </script>
 
 
-<style lang="sass">
-  .title{
+<style lang='sass'>
+.title {
     font-weight: 600;
-  }
+}
 </style>
